653001a1a3fabb5bb1de3e819f27f3f7a4a5e43e
Venkatesh17042002 <venkatesh17042002@gmail.com>
2023-06-10 14:24:30 +0530

    second commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/send-email', async (req, res) => {
                     from: "aanandhaveedu@gmail.com",
                     to: ["sankaris110p@gmail.com","ajay40@mailinator.com","venkatesh17042002@gmail.com"],
                     subject: content.subject,
-                    html: ``
+                    text: content.message
                 });
                 res.status(200).send('Email sent successfully');
             }
